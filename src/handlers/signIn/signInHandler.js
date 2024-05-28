702eaed66f3341c3b05b155edc221a7eaca266d2
Khairunas R W <128572429+khairunas001@users.noreply.github.com>
2024-05-28 22:30:50 +0700

    User can edit all his data except (gender)

---
+++ src/handlers/signIn/signInHandler.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const { PrismaClient } = require('@prisma/client');
+const JWT = require('jsonwebtoken');
+const Bcrypt = require('bcrypt');
+const createResponse = require('../createResponse');
+
+const prisma = new PrismaClient();
+const secret = process.env.JWT_SECRET;
+
+const signInHandler = async (request, h) => {
+  const { username, password } = request.payload;
+
+  const requiredFields = {
+    username,
+    password,
+  };
+
+  for (const [key, value] of Object.entries(requiredFields)) {
+    if (!value) {
+      return createResponse(h, 400, 'fail', `${key} is required to login`);
+    }
+  }
+
+  const user = await prisma.users.findFirst({
+    where: { username },
+  });
+
+  if (!user || !(await Bcrypt.compare(password, user.password))) {
+    return createResponse(h, 401, 'fail', 'Invalid username or password');
+  }
+
+  const token = JWT.sign({ id: user.id, username: user.username }, secret, { algorithm: 'HS256', expiresIn: '1d' });
+  return createResponse(h, 200, 'success', 'User has successfully logged in', { id: user.id, username: user.username, token });
+};
+
+module.exports = signInHandler;
---
+++ src/handlers/signUp/signUpLearnersHandler.js
@@ -0,0 +1,78 @@
+const bcrypt = require('bcrypt');
+const { PrismaClient } = require('@prisma/client');
+const createResponse = require('../createResponse');
+
+const prisma = new PrismaClient();
+
+const signUpLearnersHandler = async (request, h) => {
+  const {
+    email,
+    phoneNumber,
+    username,
+    password,
+    educationLevel,
+    gender,
+    domicile,
+  } = request.payload;
+
+  try {
+    // Checks for empty data in the payload
+    const requiredFields = {
+      email,
+      phoneNumber,
+      username,
+      password,
+      educationLevel,
+      gender,
+      domicile,
+    };
+
+    for (const [key, value] of Object.entries(requiredFields)) {
+      if (!value) {
+        return createResponse(h, 400, 'fail', `${key} is required to create account`);
+      }
+    }
+
+    // Check whether student data is already registered
+    const existingUser = await prisma.users.findFirst({
+      where: {
+        OR: [
+          { email },
+          { username },
+        ],
+      },
+    });
+
+    if (existingUser) {
+      return createResponse(h, 400, 'fail', 'User already registered with the provided email or username');
+    }
+
+    // Hashing user's password
+    const hash = await bcrypt.hash(password, 10);
+
+    // Save user's data to the database
+    const user = await prisma.users.create({
+      data: {
+        email,
+        username,
+        password: hash,
+      },
+    });
+
+    const learner = await prisma.learners.create({
+      data: {
+        user_id: user.id,
+        education_level: educationLevel,
+        phone_number: phoneNumber,
+        gender,
+        domicile,
+      },
+    });
+
+    return createResponse(h, 201, 'success', 'Learner registered successfully', { email: user.email, username: user.username });
+  } catch (error) {
+    return createResponse(h, 500, 'error', 'Failed to register learner due to an internal error');
+  }
+};
+
+module.exports = signUpLearnersHandler;
