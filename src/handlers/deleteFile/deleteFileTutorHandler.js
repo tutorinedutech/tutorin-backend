2763a0faae0f0aaba45328aef03278c277afb310
Khairunas R W <128572429+khairunas001@users.noreply.github.com>
2024-05-30 10:28:28 +0700

    Update user update.v3

--- src/handlers/deleteFile/deleteFileTutorHandler.js
+++ src/handlers/deleteFile/deleteFileTutorHandler.js
@@ -5,62 +5,62 @@ const createResponse = require('../createResponse');
 const prisma = new PrismaClient();
 
 const storage = new Storage({
-    projectId: process.env.GOOGLE_PROJECT_ID,
-    keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
+  projectId: process.env.GOOGLE_PROJECT_ID,
+  keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
 });
 
 const deleteFileTutorHandler = async (request, h) => {
-    try {
-        const { tutorId } = request.params;
-        const { file } = request.query;
-        const bucketName = process.env.GOOGLE_BUCKET_NAME;
+  try {
+    const { tutorId } = request.params;
+    const { file } = request.query;
+    const bucketName = process.env.GOOGLE_BUCKET_NAME;
 
-        const tutor = await prisma.tutors.findUnique({
-            where: { id: parseInt(tutorId) }
-        });
+    const tutor = await prisma.tutors.findUnique({
+      where: { id: parseInt(tutorId) },
+    });
 
-        if (!tutor) {
-            return createResponse(h, 404, 'fail', 'Tutor not found');
-        }
-
-        if (file === 'cv') {
-            const cvUrl = tutor.cv;
-            const parsedUrl = new URL(cvUrl);
-            const filePath = parsedUrl.pathname.replace(`/${bucketName}/`, '');
-
-            await storage.bucket(bucketName).file(filePath).delete().catch(err => {
-                console.error(`Failed to delete CV: ${err.message}`);
-            });
-
-            await prisma.tutors.update({
-                where: { id: parseInt(tutorId) },
-                data: { cv: null }
-            });
-
-            return createResponse(h, 200, 'success', 'CV deleted successfully');
-        }
-
-        if (file === 'profile-picture') {
-            const profilePictureUrl = tutor.profile_picture;
-            const parsedUrl = new URL(profilePictureUrl);
-            const filePath = parsedUrl.pathname.replace(`/${bucketName}/`, '');
-
-            await storage.bucket(bucketName).file(filePath).delete().catch(err => {
-                console.error(`Failed to delete Profile Picture: ${err.message}`);
-            });
-
-            await prisma.tutors.update({
-                where: { id: parseInt(tutorId) },
-                data: { profile_picture: null }
-            });
-
-            return createResponse(h, 200, 'success', 'Profile Picture deleted successfully');
-        }
-
-        return createResponse(h, 400, 'fail', 'Incorrect parameter query');
-    } catch (err) {
-        console.error(err)
+    if (!tutor) {
+      return createResponse(h, 404, 'fail', 'Tutor not found');
     }
-}
+
+    if (file === 'cv') {
+      const cvUrl = tutor.cv;
+      const parsedUrl = new URL(cvUrl);
+      const filePath = parsedUrl.pathname.replace(`/${bucketName}/`, '');
+
+      await storage.bucket(bucketName).file(filePath).delete().catch((err) => {
+        console.error(`Failed to delete CV: ${err.message}`);
+      });
+
+      await prisma.tutors.update({
+        where: { id: parseInt(tutorId) },
+        data: { cv: null },
+      });
+
+      return createResponse(h, 200, 'success', 'CV deleted successfully');
+    }
+
+    if (file === 'profile-picture') {
+      const profilePictureUrl = tutor.profile_picture;
+      const parsedUrl = new URL(profilePictureUrl);
+      const filePath = parsedUrl.pathname.replace(`/${bucketName}/`, '');
+
+      await storage.bucket(bucketName).file(filePath).delete().catch((err) => {
+        console.error(`Failed to delete Profile Picture: ${err.message}`);
+      });
+
+      await prisma.tutors.update({
+        where: { id: parseInt(tutorId) },
+        data: { profile_picture: null },
+      });
+
+      return createResponse(h, 200, 'success', 'Profile Picture deleted successfully');
+    }
+
+    return createResponse(h, 400, 'fail', 'Incorrect parameter query');
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 module.exports = deleteFileTutorHandler;
