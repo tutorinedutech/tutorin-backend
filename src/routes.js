68f4118b6561c729507493215b27ed6a938be2ab
Khairunas R W <128572429+khairunas001@users.noreply.github.com>
2024-06-13 22:05:53 +0700

    update to https and add feature to show own users profile

--- src/routes.js
+++ src/routes.js
@@ -162,12 +162,6 @@ const routes = [
     path: '/tutors/{tutorId}/profile',
     handler: deleteFileTutorHandler,
   },
-  // untuk menghapus pending payments yang sudah expired
-  // {
-  //   method: 'DELETE',
-  //   path: '/delete-pending-payments',
-  //   handler: deleteOldPendingPayments,
-  // },
   // untuk melakukan transaksi
   {
     method: 'POST',
